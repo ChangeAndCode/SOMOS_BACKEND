30c6afe411e1ecd3a56f7a3891f6c9ae6d8f8dc2
Gibrán Pérez <gperezfavela@gmail.com>
2025-09-11 14:00:59 -0600

    added register API

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { loginUser, getProfile } from "../controllers/auth.js";
+import { loginUser, getProfile, registerUser } from "../controllers/auth.js";
 import { authMiddleware } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.get("/me", authMiddleware, getProfile); // Ruta protegida
+router.post("/register", registerUser);
 
 export default router;
